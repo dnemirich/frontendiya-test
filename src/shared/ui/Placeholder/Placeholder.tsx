cfe7c741d14bb76ebe31143b9986f7a1deb54be6
dnemirich <dnemirich@gmail.com>
2025-06-12 00:42:07 +0200

    feat: add placeholder component for the main page

---
+++ src/shared/ui/Placeholder/Placeholder.tsx
@@ -0,0 +1,26 @@
+import { LayoutContainer } from '../LayoutContainer/LayoutContainer.tsx';
+import search from '/icons/searchIcon.svg';
+import user from '/icons/userIcon.svg';
+
+import s from './placeholder.module.scss';
+
+type Props = {
+  type: 'idle' | 'not-found';
+};
+
+export const Placeholder = ({ type }: Props) => {
+  return (
+    <LayoutContainer className={s.container}>
+      <div className={s.wrapper}>
+        <div className={s.iconWrapper}>
+          {type === 'idle' && <img src={search} alt="search icon" className={s.icon} />}
+          {type === 'not-found' && <img src={user} alt="user icon" className={s.icon} />}
+        </div>
+        <p className={s.text}>
+          {type === 'idle' && 'Start with searching a GitHub user'}
+          {type === 'not-found' && 'User not found'}
+        </p>
+      </div>
+    </LayoutContainer>
+  );
+};
