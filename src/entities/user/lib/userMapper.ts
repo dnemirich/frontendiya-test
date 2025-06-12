9f6edd8f4a4da56d96437bc91a06acb2b6df1f21
dnemirich <dnemirich@gmail.com>
2025-06-12 21:09:48 +0200

    feat: add auxiliary functions for user fetching and mapping

---
+++ src/entities/user/lib/userMapper.ts
@@ -0,0 +1,26 @@
+import type { User, UserShort } from '../model';
+import { formatNumber } from './numberFormatter.ts';
+
+export const userMapper = (user: User): UserShort => {
+  const formatFollowersText = (count: number): string => {
+    if (count === 0) return 'No followers';
+    if (count === 1) return '1 follower';
+    return `${formatNumber(count)} followers`;
+  };
+
+  // Функция для обработки подписок
+  const formatFollowingText = (count: number): string => {
+    if (count === 0) return 'Not following anyone';
+    if (count === 1) return '1 following';
+    return `${formatNumber(count)} following`;
+  };
+
+  return {
+    bio: user.bio,
+    html_url: user.html_url,
+    avatar_url: user.avatar_url,
+    login: user.login,
+    following: formatFollowingText(user.following),
+    followers: formatFollowersText(user.followers),
+  };
+};
